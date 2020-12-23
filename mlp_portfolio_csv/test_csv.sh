b6af7bf443a44ab6d4f47f90a88efb78719ed91e
Stefan Krone <stefan.krone@zalando.de>
2020-12-23 15:54:01 +0100

    adjust .gitignore

--- mlp_portfolio_csv/test_csv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp data/converted/archiv/* data/examples
-rm data/converted/* -r
-
-python convert_to_portfolio_csv.py -f /workspaces/portfolio_performance_utils/mlp_portfolio_csv/data/examples \
-                        -m /workspaces/portfolio_performance_utils/mlp_portfolio_csv/data/converted \
-                        -b /workspaces/portfolio_performance_utils/mlp_portfolio_csv/bank_accounts.json
